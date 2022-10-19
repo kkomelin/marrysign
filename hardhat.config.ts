9ac650b79256bdc0521392d7ab5b890f70fe041f
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-10-19 18:29:13 +0300

    Added Prettier configs. Changed default typechain folder. Formated TS code according to my standards.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox'
+import { HardhatUserConfig } from 'hardhat/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.17',
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+}
 
-export default config;
+export default config
