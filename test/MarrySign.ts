e8439a95e1142ad7efd12775b3ffc0127436826d
Konstantin Komelin <konstantin.komelin@gmail.com>
2022-10-20 06:56:56 +0300

    Used beforeEach hook to deploy the contract before any test case run.

--- test/MarrySign.ts
+++ test/MarrySign.ts
@@ -1,12 +1,28 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { expect } from 'chai'
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { ethers } from 'hardhat'
+import { MarrySign } from '../typechain'
 
 const TEST_MESSAGE = 'Test message'
 
-describe('MarrySign', function () {
-  async function deployMarrySignFixture() {
-    // Contracts are deployed using the first signer/account by default
+describe('MarrySign', () => {
+  let contract: MarrySign
+  let owner: SignerWithAddress
+  let otherAccount: SignerWithAddress
+
+  beforeEach(async () => {
+    const fixtureResults = await loadFixture(_deployContractFixture)
+
+    contract = fixtureResults.contract
+    owner = fixtureResults.owner
+    otherAccount = fixtureResults.otherAccount
+
+    return fixtureResults
+  })
+
+  const _deployContractFixture = async () => {
+    // Contracts are deployed using the first signer/account by default.
     const [owner, otherAccount] = await ethers.getSigners()
 
     const MarrySignContract = await ethers.getContractFactory('MarrySign')
@@ -17,8 +33,6 @@ describe('MarrySign', function () {
 
   describe('Deployment', async () => {
     it('Should set correct message', async () => {
-      const { contract } = await loadFixture(deployMarrySignFixture)
-
       expect(await contract.getMessage()).to.equal(TEST_MESSAGE)
     })
   })
@@ -26,19 +40,13 @@ describe('MarrySign', function () {
   describe('#setMessage', async () => {
     describe('on failure', () => {
       it('Should revert if the message is empty', async () => {
-        const { contract } = await loadFixture(deployMarrySignFixture)
-
         const newMessage = ''
 
-        await expect(contract.setMessage(newMessage)).to.be.revertedWith(
+        await expect(contract.connect(owner).setMessage(newMessage)).to.be.revertedWith(
           'Message cannot be empty'
         )
       })
       it('Should revert if the caller is not the owner', async () => {
-        const { contract, otherAccount } = await loadFixture(
-          deployMarrySignFixture
-        )
-
         const newMessage = 'New message'
 
         await expect(
@@ -48,8 +56,6 @@ describe('MarrySign', function () {
     })
     describe('on success', async () => {
       it('Should set the correct message', async () => {
-        const { contract } = await loadFixture(deployMarrySignFixture)
-
         const newMessage = 'New message'
 
         await contract.setMessage(newMessage)
